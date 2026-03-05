5aec36ebddfbd16ba95f8fb70a9d76adfa93ff0b
eknabevcc <emil.knabe@volvocars.com>
2026-03-05 21:11:56 +0100

    attempt 4

---
+++ pbtest.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Google.Protobuf;
+
+namespace PBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
